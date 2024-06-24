e4f344f359452f737bbd57ce0da7b0ef3ff7b44b
FranckMenil <franckmenil@gmail.com>
2024-06-24 17:03:37 +0200

    add variables

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 console.log("connecté !")
+
+// Séléctionner et stocker les éléments du formulaire
+const form = document.querySelector("form");
+const firstName = document.getElementById("first-name");
+const lastName = document.getElementById("last-name");
+const message = document.getElementById("message");
+
+// Séléctionner et stocker le message d'erreur
+const errorMessage = document.getElementById("error-message");
