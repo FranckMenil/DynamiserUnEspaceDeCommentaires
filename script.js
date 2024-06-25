3e3fba6b554a897dfc4a37d6b69537ad28a87c01
FranckMenil <franckmenil@gmail.com>
2024-06-25 15:30:51 +0200

    add function

--- script.js
+++ script.js
@@ -8,3 +8,28 @@ const message = document.getElementById("message");
 
 // Séléctionner et stocker le message d'erreur
 const errorMessage = document.getElementById("error-message");
+
+
+
+// Mettre des fonctions au formulaire 
+form.addEventListener("submit" , function(e){
+
+    // Empêcher le raffraichissement de la page
+    e.preventDefault();
+
+    // Récupérer les valeurs des inputs
+    const firstNameValue = firstName.value;
+    const lastNameValue = lastName.value;
+    const messageValue = message.value;
+
+    // Si un des champs est vide , afficher un message d'erreur sinon le laisser caché
+    if (firstNameValue ==="") {
+        errorMessage.style.display = "block";
+    } else if (lastNameValue ==="") {
+        errorMessage.style.display = "block";
+    } else if (messageValue ==="") {
+        errorMessage.style.display = "block";
+    } else {
+        errorMessage.style.display = "none";
+    }
+});
